02fdebb5bfee17e270c6f8f473cde8fbca093faf
Mouazzama-dev <mouazzamabinteumer.ep19102044@gmail.com>
2024-10-30 17:09:13 +0500

    email setup

--- src/prices/prices.module.ts
+++ src/prices/prices.module.ts
@@ -3,9 +3,27 @@ import { HttpModule } from '@nestjs/axios';
 import { ScheduleModule } from '@nestjs/schedule';
 import { PricesController } from './prices/prices.controller';
 import { PricesService } from './prices/prices.service';
+import { MailerModule } from '@nestjs-modules/mailer';
+
 
 @Module({
-  imports: [ScheduleModule.forRoot(), HttpModule],
+  imports: [
+    MailerModule.forRoot({
+      transport: {
+        host: 'sandbox.smtp.mailtrap.io', // Replace with your SMTP host
+        port: 25,
+        auth: {
+          user: '97a3fe8bc8ef4c', // Replace with your email
+          pass: '8124803d28d09b',    // Replace with your email password
+        },
+      },
+      defaults: {
+        from: '"No Reply" <no-reply@example.com>', // Replace with a default sender address
+      },
+    }),
+    ,ScheduleModule.forRoot(), 
+    HttpModule
+  ],
   controllers: [PricesController],
   providers: [PricesService]
 })
